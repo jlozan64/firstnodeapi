9e5b5c0bc247012332918024bd2f00c655ca6257
jlozan64 <janeth2.jl@gmail.com>
2020-03-28 21:03:30 -0500

    finished through 2nd video

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import './helpers/dotenv'
+
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import pino from 'express-pino-logger'
+
+import { notFound, errorHandler } from './helpers/errors'
+
+const app = express()
+const port = parseInt(process.env.PORT)
+
+app.use(cors({ origin: process.env.ORIGIN }))
+app.use(helmet())
+app.use(pino())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'Hello There'})
+})
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port)
